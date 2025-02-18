4e9fee2b0380cb268b21bdc35208ed7e0e8a3064
mthunz <fakemggg@gmail.com>
2025-02-18 13:30:26 +0700

    auto

--- js/MOC_Locket.js
+++ js/MOC_Locket.js
@@ -16,13 +16,13 @@ const mocgarden = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: new Date().toISOString(),
-  purchase_date: new Date().toISOString(),
+  original_purchase_date: "2024-07-28T01:04:18Z",
+  purchase_date: "2024-07-28T01:04:17Z",
   store: "app_store",
 };
 const mthunz = {
   grace_period_expires_date: null,
-  purchase_date: new Date().toISOString(),
+  purchase_date: "2024-07-28T01:04:17Z",
   product_identifier: "com.mocgarden.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z",
 };
@@ -30,7 +30,7 @@ const mthunz = {
 const match = Object.keys(mapping).find((e) => ua.includes(e));
 
 if (match) {
-  const [e, s] = mapping[match];
+  let [e, s] = mapping[match];
 
   if (s) {
     mthunz.product_identifier = s;
