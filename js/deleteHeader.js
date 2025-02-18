205ccff5bdd848fe8255759411980df9d642579f
mthunz <fakemggg@gmail.com>
2025-02-18 17:22:43 +0700

    auto

--- js/deleteHeader.js
+++ js/deleteHeader.js
@@ -1,9 +1,19 @@
-const headers = $request.headers;
+// const headers = $request.headers;
 
-const key = "X-RevenueCat-ETag";
-const lkey = key.toLocaleLowerCase();
+// const key = "X-RevenueCat-ETag";
+// const lkey = key.toLocaleLowerCase();
 
-if (lkey in headers) headers[lkey] = "";
-else headers[key] = "";
+// if (lkey in headers) headers[lkey] = "";
+// else headers[key] = "";
 
-$done({ headers });
+// $done({ headers });
+
+const version = "V1.0.2";
+
+function setHeaderValue(e, a, d) {
+    var r = a.toLowerCase();
+    r in e ? (e[r] = d) : (e[a] = d);
+}
+var modifiedHeaders = $request.headers;
+
+setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", ""), $done({ headers: modifiedHeaders });
